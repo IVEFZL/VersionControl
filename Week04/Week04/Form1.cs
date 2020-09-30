9bb1d4b3fa1f38ec54f58f72ff3a9aaf024ff1cb
Tibor Sirkó <tibor.sirko@stud.uni-corvinus.hu>
2020-09-30 18:50:40 +0200

    Added data export

--- Week04/Week04/Form1.cs
+++ Week04/Week04/Form1.cs
@@ -32,6 +32,72 @@ namespace Week04
             Flats = context.Flats.ToList();
         }
 
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+            "Kód",
+            "Eladó",
+            "Oldal",
+            "Kerület",
+            "Lift",
+            "Szobák száma",
+            "Alapterület (m2)",
+            "Ár (mFt)",
+            "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 1; i < headers.Length+1; i++)
+            {
+                xlSheet.Cells[i, 1] = headers[0];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            for (int i = 2; i < values.GetLength(0)+2; i++)
+            {
+                string price = "";
+                string area = "";
+                price = GetCell(i, 8);
+                area = GetCell(i, 7);
+                xlSheet.Cells[i, 9] = "=" + price + "/" + area;
+            }
+        }
+
         private void CreateExcel()
         {
             try
