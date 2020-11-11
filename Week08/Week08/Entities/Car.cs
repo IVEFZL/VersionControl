fe38574eac716f2602f1fca391182b6acd9519aa
Tibor Sirkó <tibor.sirko@stud.uni-corvinus.hu>
2020-11-11 14:18:19 +0100

    Added car.png new

--- Week08/Week08/Entities/Car.cs
+++ Week08/Week08/Entities/Car.cs
@@ -12,7 +12,7 @@ namespace Week08.Entities
     {
         protected override void DrawImage(Graphics g)
         {
-            Image imageFile = Image.FromFile("Images/car.png");
+            Image imageFile = Image.FromFile("Image/car.png");
             g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
