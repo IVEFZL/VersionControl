f759b331076b69dd28f816fc3da539446a18d9f9
Tibor Sirkó <tibor.sirko@stud.uni-corvinus.hu>
2020-11-18 11:21:38 +0100

    Password test

--- Week09/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ Week09/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -11,7 +11,11 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [TestCase("abcd1234", false),
+         TestCase("irf@uni-corvinus", false),
+         TestCase("irf.uni-corvinus.hu", false),
+         TestCase("irf@uni-corvinus.hu", true)
+            ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -19,6 +23,23 @@ namespace UnitTestExample.Test
 
             // Act
             var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestCase("ABCD1234", false),
+         TestCase("Ab1234", false),
+         TestCase("Abcd1234", true),
+         TestCase("abcd1234", false),
+         TestCase("abcdABCD", false)
+            ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
